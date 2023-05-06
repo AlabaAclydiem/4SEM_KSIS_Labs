853425fc37a0909f2d4bfd1add7167c3a2502d94
AlabaAclydiem <92473617+AlabaAclydiem@users.noreply.github.com>
2023-05-06 20:44:52 +0300

    CourseWork started!

--- Lab 3/client.py
+++ Lab 3/client.py
@@ -1,8 +1,108 @@
+import http.client
 from http.client import HTTPConnection
+import os
 
-connection = HTTPConnection('127.0.0.1', 9090, timeout=10)
-connection.request("GET", "/")
-response = connection.getresponse()
-print(response.read())
+ROOT = "client_directory"
 
-connection.close()
+
+def main():
+    host, port = input("Введите адрес сервера: ").strip(), int(input("Введите порт сервера: ").strip())
+    connection = HTTPConnection(host, port, timeout=1)
+    print("Клиент подключён", "Вы можете использовать команды для взаимодействия с файловым хранилищем",
+          "Введите HELP для получения списка доступных команд", sep="\n", end="\n\n\n")
+    try:
+        while True:
+            command = input(">> ").strip().split()
+            if command[0] == "HELP":
+                print("Синтаксис доступных команд:",
+                      "GET {server_file_path}",
+                      "Загрузить заданный файл с сервера",
+                      "",
+                      "POST {server_file_path} {client_file_path|text_string}",
+                      "Добавить содержимое файла|строку в конец файла на сервере",
+                      "",
+                      "PUT {server_file_path|server_directory_path} [file_path|text_string|'-d']",
+                      "Перезаписать|создать файл на сервере содержимым файла|пустой или непустой строкой",
+                      "Создать пустую директорию",
+                      "",
+                      "MOVE {server_file_path_from|server_dir_path_from} {server_file_path_to}",
+                      "Переместить файл на серевере",
+                      "",
+                      "COPY {server_file_path_from|server_dir_path_from} {server_dir_path_to}",
+                      "Скопировать файл на сервере",
+                      "",
+                      "DELETE {server_file_path|server_dir_path}",
+                      "Удалить файл на сервере",
+                      "",
+                      "HIERARCHY",
+                      "Вывести структуру файловой системы сервера",
+                      "",
+                      "HELP",
+                      "Показать справку о командах клиента",
+                      "",
+                      "EXIT",
+                      "Завершить сессию",
+                      sep="\n\t", end="\n\n\n")
+                continue
+            elif command[0] == "GET":
+                connection.request("GET", "/get", body=f"{command[1]}")
+                response = connection.getresponse()
+                msg, filename, filedata = response.read().split("<$$>".encode())
+                with open(f"./client_directory/{filename.decode()}", "wb") as f:
+                    f.write(filedata)
+                print(msg.decode(), end="\n\n\n")
+                continue
+            elif command[0] == "POST":
+                with open("temp.txt", "wb+") as f:
+                    f.write(f"{command[1]}<$$>".encode())
+                    try:
+                        with open(command[2], "rb") as file:
+                            f.write(file.read())
+                    except FileNotFoundError:
+                        f.write(command[2].encode())
+                    except IsADirectoryError:
+                        print("Директория не может служить источником данных для записи", end="\n\n\n")
+                        continue
+                    f.seek(0, os.SEEK_SET)
+                    connection.request("POST", "/post", body=f)
+                os.system("rm temp.txt")
+            elif command[0] == "PUT":
+                if len(command) == 2:
+                    command.append("")
+                with open("temp.txt", "wb+") as f:
+                    f.write(f"{command[1]}<$$>".encode())
+                    try:
+                        with open(command[2], "rb") as file:
+                            f.write(file.read())
+                    except FileNotFoundError:
+                        f.write(command[2].encode())
+                    except IsADirectoryError:
+                        print("Директория не может служить источником данных для записи", end="\n\n\n")
+                        continue
+                    f.seek(0, os.SEEK_SET)
+                    connection.request("PUT", "/put", body=f)
+                os.system("rm temp.txt")
+            elif command[0] == "COPY":
+                connection.request("COPY", "/copy", body=f"{command[1]} {command[2]}")
+            elif command[0] == "MOVE":
+                connection.request("MOVE", "/move", body=f"{command[1]} {command[2]}")
+            elif command[0] == "DELETE":
+                connection.request("DELETE", "/delete", body=command[1])
+            elif command[0] == "HIERARCHY":
+                connection.request("HIERARCHY", "/hierarchy")
+            elif command[0] == "EXIT":
+                break
+            try:
+                response = connection.getresponse()
+            except http.client.ResponseNotReady:
+                pass
+            else:
+                print(response.read().decode(), end="\n\n\n")
+    except KeyboardInterrupt:
+        pass
+    connection.close()
+    print("Сессия завершена")
+
+
+if __name__ == "__main__":
+    main()
--- Lab 3/server.py
+++ Lab 3/server.py
@@ -1,20 +1,183 @@
 from http.server import HTTPServer, BaseHTTPRequestHandler
+import os
+import shutil
 
-HOST = "127.0.0.1"
-PORT = 9090
 ROOT = "server_directory"
 
 
 class CustomHTTPRequestHandler(BaseHTTPRequestHandler):
-    def do_GET(self):
+    def basic_headers(self):
         self.send_response(200)
-        self.send_header('Content-type', 'text/html')
         self.end_headers()
 
-        with open(f"{ROOT}/test.txt", "rb") as file:
-            self.wfile.write(file.read())
+    def read_data(self):
+        if "Content-Length" in self.headers:
+            content_length = int(self.headers["Content-Length"])
+            body = self.rfile.read(content_length)
+            return body
+        elif "chunked" in self.headers.get("Transfer-Encoding", ""):
+            body = bytearray()
+            while True:
+                line = self.rfile.readline().strip()
+                chunk_length = int(line, 16)
+                if chunk_length != 0:
+                    chunk = self.rfile.read(chunk_length)
+                    body += chunk
+                self.rfile.readline()
+                if chunk_length == 0:
+                    break
+            return body
+        return bytes()
+
+    def do_GET(self):
+        self.basic_headers()
+        file = f"./{ROOT}/{self.read_data().decode()}"
+        try:
+            with open(file, "rb") as f:
+                self.wfile.write("Файл успешно получен<$$>".encode())
+                self.wfile.write(f"{file.split('/')[-1]}<$$>".encode())
+                self.wfile.write(f.read())
+        except FileNotFoundError:
+            self.wfile.write("Запрашиваемого файла не существует".encode())
+        except IsADirectoryError:
+            self.wfile.write("Нельзя запросить директорию".encode())
+
+    def do_POST(self):
+        self.basic_headers()
+        file, data = self.read_data().decode().split("<$$>")
+        try:
+            with open(f"./{ROOT}/{data}", "rb") as f:
+                temp = f.read().decode()
+            data = temp
+        except (FileNotFoundError, IsADirectoryError):
+            pass
+        file = f"./{ROOT}/{file}"
+        try:
+            with open(file, "rb"):
+                pass
+        except FileNotFoundError:
+            self.wfile.write("Файла для записи не существует".encode())
+        except IsADirectoryError:
+            self.wfile.write("Нельзя записать в директорию".encode())
+        else:
+            with open(file, "ab") as f:
+                f.write(data.encode())
+            self.wfile.write("Данные успешно записаны".encode())
+
+    def do_PUT(self):
+        self.basic_headers()
+        file, data = self.read_data().decode().split("<$$>")
+        try:
+            with open(f"./{ROOT}/{data}", "rb") as f:
+                temp = f.read().decode()
+            data = temp
+        except (FileNotFoundError, IsADirectoryError):
+            pass
+        file = f"./{ROOT}/{file}"
+        try:
+            with open(file, "rb"):
+                pass
+        except FileNotFoundError:
+            if data != "-d":
+                with open(file, "wb") as f:
+                    f.write(data.encode())
+                self.wfile.write("Файл создан, данные успешно записаны".encode())
+            else:
+                os.mkdir(file)
+                self.wfile.write("Директория создана".encode())
+        except IsADirectoryError:
+            if data != "-d":
+                self.wfile.write("Нельзя перезаписать директорию".encode())
+            else:
+                self.wfile.write("Директория с данным путём уже есть".encode())
+        else:
+            with open(file, "wb") as f:
+                f.write(data.encode())
+            self.wfile.write("Данные успешно перезаписаны".encode())
+
+    def do_COPY(self):
+        self.basic_headers()
+        files = self.read_data().decode().split()
+        files = tuple(map(lambda x: f"./{ROOT}/{x}" if x != "." else f"./{ROOT}", files))
+        try:
+            with open(files[1], "rb"):
+                pass
+        except FileNotFoundError:
+            self.wfile.write("Целевой директории не существует".encode())
+        except IsADirectoryError:
+            try:
+                with open(files[0], "rb"):
+                    pass
+            except FileNotFoundError:
+                self.wfile.write("Копируемые файл или директрия не существуют".encode())
+            except IsADirectoryError:
+                os.system(f"cp -r {files[0]} {files[1]}")
+                self.wfile.write("Директория успешно скопирована".encode())
+            else:
+                shutil.copy2(files[0], files[1])
+                self.wfile.write("Файл успешно скопирован".encode())
+        else:
+            self.wfile.write("Копирование в файл невозможно".encode())
+
+    def do_MOVE(self):
+        self.basic_headers()
+        files = self.read_data().decode().split()
+        files = tuple(map(lambda x: f"./{ROOT}/{x}" if x != "." else f"./{ROOT}", files))
+        try:
+            with open(files[1], "rb"):
+                pass
+        except FileNotFoundError:
+            self.wfile.write("Целевой директории не существует".encode())
+        except IsADirectoryError:
+            try:
+                with open(files[0], "rb"):
+                    pass
+            except FileNotFoundError:
+                self.wfile.write("Перемещаемые файл или директрия не существуют".encode())
+            except IsADirectoryError:
+                shutil.move(files[0], files[1])
+                self.wfile.write("Директория успешно перемещена".encode())
+            else:
+                shutil.move(files[0], files[1])
+                self.wfile.write("Файл успешно перемещён".encode())
+        else:
+            self.wfile.write("Перемещение в файл невозможно".encode())
+
+    def do_DELETE(self):
+        self.basic_headers()
+        file = f"./{ROOT}/{self.read_data().decode()}"
+        try:
+            with open(file, "rb"):
+                pass
+        except FileNotFoundError:
+            self.wfile.write("Удаляемые файл или директория не существуют".encode())
+        except IsADirectoryError:
+            shutil.rmtree(file)
+            self.wfile.write("Директория успешно удалена".encode())
+        else:
+            os.system(f"rm {file}")
+            self.wfile.write("Файл успешно удалён".encode())
+
+    def do_HIERARCHY(self):
+        self.basic_headers()
+        os.system(f"tree {ROOT} >> ./text.txt")
+        with open("text.txt", "rb") as f:
+            self.wfile.write(f.read())
+        os.system(f"rm ./text.txt")
 
 
-server = HTTPServer((HOST, PORT), CustomHTTPRequestHandler)
-server.serve_forever()
-server.server_close()
+def main():
+    host, port = input("Введите адрес сервера: ").strip(), int(input("Введите порт сервера: ").strip())
+    server = HTTPServer((host, port), CustomHTTPRequestHandler)
+    print("Сервер запущен")
+    try:
+        server.serve_forever()
+    except KeyboardInterrupt:
+        pass
+    finally:
+        server.server_close()
+        print("Сервер выключен")
+
+
+if __name__ == "__main__":
+    main()
